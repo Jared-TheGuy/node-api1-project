8fbca2a43581d167e0960285c14781e0aafe2c0c
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-11-29 19:29:43 -0700

    Created test endpoint to ensure server can consume client requests

--- api/server.js
+++ api/server.js
@@ -7,4 +7,24 @@ const server = express();
 
 server.use(express.json())
 
+//EndPoints
+
+server.get('/api/test', (req, res) => {
+    res.json({boong: 'bong'})
+})
+
+// Get all users endpoint
+
+// server.get('/api/users', (req, res) => {
+//     try {
+//         const users = await Users.find()
+//     }
+//     catch (err) {
+//         res.status(500).json({
+//             message: "something bad happened",
+//             error: err.message
+//         })
+//     }
+// })
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
