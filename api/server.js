f887500f1ebde1f28b19d68901bc957734053bca
Jared Bates-Baker <51772046+Jared-TheGuy@users.noreply.github.com>
2021-11-29 19:34:55 -0700

    Created get all users endpoint

--- api/server.js
+++ api/server.js
@@ -9,22 +9,23 @@ server.use(express.json())
 
 //EndPoints
 
-server.get('/api/test', (req, res) => {
-    res.json({boong: 'bong'})
-})
+// server.get('/api/test', (req, res) => {
+//     res.json({boong: 'bong'})
+// })
 
 // Get all users endpoint
 
-// server.get('/api/users', (req, res) => {
-//     try {
-//         const users = await Users.find()
-//     }
-//     catch (err) {
-//         res.status(500).json({
-//             message: "something bad happened",
-//             error: err.message
-//         })
-//     }
-// })
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await Users.find()
+        res.json(users)
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "something bad happened",
+            error: err.message
+        })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
